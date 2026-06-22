41112db9f42920b0e34b064b49218b1b6d98d8ef
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2026-06-22 23:44:46 +0500

    style: improve code formatting and consistency in helloPage function

--- Next_js/project-1/app/(dashboard)/todos/page.js
+++ Next_js/project-1/app/(dashboard)/todos/page.js
@@ -1,15 +1,15 @@
 import TodoList from "@/components/TodoList";
 
 
-function helloPage(){
+function helloPage() {
 
-    const todos = [{_id : 1 ,content : "does coding" }, {_id : 2 ,content : 'excersice'} , {_id : 3 ,content : "namaz"} , {_id : 4 ,content : "talawat"},]
-    return(
+    const todos = [{ _id: 1, content: "does coding" }, { _id: 2, content: 'excersice' }, { _id: 3, content: "namaz" }, { _id: 4, content: "talawat" },]
+    return (
         <>
-        <div>Todes Page</div>
-        <TodoList todos = {todos}/>
+            <div>Todes Page</div>
+            <TodoList todos={todos} />
         </>
-        
+
     )
 }
 
